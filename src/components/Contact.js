5ed18a94c7aaed99bf1443410efaa27d504e5942
Mostafa Ghadimi <mostafa.ghadimi@yahoo.com>
2023-03-21 18:00:28 +0330

    :ocean: Add Contact component to the App

---
+++ src/components/Contact.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+
+function Contact() {
+    return (
+        <section id="contact-section" class="inactive" style={{"display": "none"}}>
+            <p id="text01">
+                Contact
+            </p>
+            <p id="text07">
+                Here are the ways to get in touch with me:
+            </p>
+            <p id="text02">
+                <span class="p">
+                    <a href="https://linkedin.com/in/mostafaghadimi">
+                        My Linkedin Profile
+                    </a>
+                    <br/>
+                    <a href="mailto:mostafa.ghadimi@yahoo.com">
+                        mostafa.ghadimi@yahoo.com
+                    </a>
+                </span>
+            </p>
+        </section>
+    )
+}
+
+export default Contact;
